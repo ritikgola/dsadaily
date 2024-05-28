2bbce7d9aa38b128e6ba6544cb8f8849e7f853d8
ritikgola <145039838+ritikgola@users.noreply.github.com>
2024-05-28 14:49:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-Palindrome/kpalindrome.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(input_line[0]);
+            int k = Integer.parseInt(input_line[1]);
+            String str = in.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.kPalindrome(str, n, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int kPalindrome(String str, int n, int k) {
+        // code here
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i < n - length + 1; i++) {
+                int j = i + length - 1;
+                if (str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        int lpsLength = dp[0][n - 1];
+        int minDeletions = n - lpsLength;
+        return minDeletions <= k ? 1 : 0;
+    }
+}
