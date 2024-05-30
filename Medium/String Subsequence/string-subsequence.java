ee05cf35bba953b2e98a0e35c9005db33d44dd3e
ritikgola <145039838+ritikgola@users.noreply.github.com>
2024-05-30 18:56:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/String Subsequence/string-subsequence.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String s1;
+            s1 = br.readLine();
+
+            String s2;
+            s2 = br.readLine();
+
+            Solution obj = new Solution();
+            int res = obj.countWays(s1, s2);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int countWays(String s1, String s2) {
+        // code here
+        int n = s1.length();
+        int m = s2.length();
+        int MOD = 1000000007;
+        
+        int[][] dp = new int[n + 1][m + 1];
+        
+        // Initialize dp array
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // There's exactly one way to match an empty s2
+        }
+        
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+                } else {
+                    dp[i][j] = dp[i - 1][j] % MOD;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
