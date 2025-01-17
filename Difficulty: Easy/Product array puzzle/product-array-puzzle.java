3e5a04f9819f20fd852072325dab7f3b68a07fe7
ritikgola <145039838+ritikgola@users.noreply.github.com>
2025-01-17 23:34:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         while (t > 0) {
-            int n = sc.nextInt();
-            int[] array = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
             Solution ob = new Solution();
-            long[] ans = new long[n];
-            ans = ob.productExceptSelf(array);
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
 
             for (int i = 0; i < n; i++) {
                 System.out.print(ans[i] + " ");
             }
             System.out.println();
+            System.out.println("~");
             t--;
         }
     }
@@ -31,25 +33,25 @@ class GFG {
 
 
 // User function Template for Java
-
 class Solution {
-    public static long[] productExceptSelf(int nums[]) {
+    public static int[] productExceptSelf(int arr[]) {
         // code here
-        int n = nums.length;
-        long[] leftProduct = new long[n];
-        long[] result = new long[n];
+        int n=arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] res = new int[n];
+        left[0]=1;
+        for(int i=1; i<n; i++){
+            left[i]=left[i-1]*arr[i-1];
+        }
+        right[n-1]=1;
+        for(int i=n-2; i>=0; i--){
+           right[i]=right[i+1]*arr[i+1]; 
+        }
         
-        leftProduct[0] = 1;
-        for (int i = 1; i < n; i++) {
-            leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+        for(int i=0; i<n; i++){
+           res[i]=left[i]*right[i]; 
         }
-
-        long rightProductAcc = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] = leftProduct[i] * rightProductAcc;
-            rightProductAcc *= nums[i];
-        }
-
-        return result;
+        return res;
     }
 }
